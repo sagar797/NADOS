0e851dbfe5c732ce178edfd3e6dc2e21bba95d6f
sagar gupta <gupta.sagar797@gmail.com>
2022-10-08 00:08:56 +0530

    easy peasy

---
+++ Dynamic Programming(Beginners)/fibonacci_dp.java
@@ -0,0 +1,17 @@
+public class fibonacci_dp {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(solution(n));
+    }
+
+    private static int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
