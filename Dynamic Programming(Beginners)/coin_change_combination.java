6c98439c53186076581eeb1b6e8fffeacaf60af0
sagar gupta <gupta.sagar797@gmail.com>
2022-10-09 19:56:57 +0530

    permutation => use coin for every target combination => for every target use coin to avoid using it later which generates permutation

---
+++ Dynamic Programming(Beginners)/coin_change_combination.java
@@ -0,0 +1,21 @@
+public class coin_change_combination {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = { 2, 3, 5, 6 };
+        int amount = 7;
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int coin = arr[i];
+            for (int j = 1; j <= amount; j++) {
+                if (j - coin >= 0) {
+                    dp[j] += dp[j - coin];
+                }
+            }
+        }
+
+        System.out.println(dp[amount]);
+    }
+}
---
+++ Dynamic Programming(Beginners)/coin_change_permutations.java
@@ -0,0 +1,21 @@
+public class coin_change_permutations {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = { 2, 3, 5, 6 };
+        int amount = 7;
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                int coin = arr[j];
+                if (i - coin >= 0) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+        }
+
+        System.out.println(dp[amount]);
+    }
+}
