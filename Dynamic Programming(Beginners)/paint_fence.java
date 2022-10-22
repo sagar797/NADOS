6f22bc763670ce32e7de35b39b039faecb805d07
sagar gupta <gupta.sagar797@gmail.com>
2022-10-22 19:36:46 +0530

    todo again noice question

--- Dynamic Programming(Beginners)/paint_fence.java
+++ Dynamic Programming(Beginners)/paint_fence.java
@@ -3,6 +3,25 @@ public class paint_fence {
         int n = 4;
         int k = 3;
 
+        onSpaceApproach(n, k);
+        constantSpaceApproach(n, k);
+    }
+
+    private static void constantSpaceApproach(int n, int k) {
+        int same = k;
+        int diff = k * (k - 1);
+        int total = same + diff;
+
+        for (int i = 3; i <= n; i++) {
+            same = diff;
+            diff = total * (k - 1);
+            total = same + diff;
+        }
+
+        System.out.println(total);
+    }
+
+    public static void onSpaceApproach(int n, int k) {
         int[][] dp = new int[3][n + 1];
         dp[0][2] = k;
         dp[1][2] = k * (k - 1);
